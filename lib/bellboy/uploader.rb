88cc6415d9e31f13c6edc5a234ffc8cb9ce5fe09
Kristian Van Der Vliet <kvandervliet@dyn.com>
2014-01-15 14:29:23 +0000

    JSON -> json

--- lib/bellboy/uploader.rb
+++ lib/bellboy/uploader.rb
@@ -16,7 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 require 'bellboy'
-require 'JSON'
+require 'json'
 
 module Bellboy
   # Push data bags & data bag items to the Chef server
